ccf03dfeee11da3f9d24e8777548a2d588bc4483
K-Pomian <pomian.konrad@gmail.com>
2022-10-25 12:00:43 +0200

    Add MarketplaceOffer

--- crates/indexer/src/models/marketplace_models/offers.rs
+++ crates/indexer/src/models/marketplace_models/offers.rs
@@ -1,11 +1,15 @@
 #![allow(clippy::extra_unused_lifetimes)]
 #![allow(clippy::unused_unit)]
 
-use aptos_api_types::{TransactionPayload, UserTransaction};
+use anyhow::Result;
+use aptos_api_types::{EntryFunctionPayload, TransactionPayload, WriteTableItem};
+use aptos_types::transaction::TransactionPayload;
 use field_count::FieldCount;
 use serde::{Deserialize, Serialize};
 
-use crate::{schema::marketplace_offers, util::parse_timestamp};
+use crate::schema::marketplace_offers;
+
+use super::utils::{MarketplacePayload, MarketplaceWriteSet};
 
 #[derive(Debug, Deserialize, FieldCount, Identifiable, Insertable, Queryable, Serialize)]
 #[diesel(primary_key(creator_address, collection_name))]
@@ -14,30 +18,54 @@ pub struct MarketplaceOffer {
     creator_address: String,
     collection_name: String,
     token_name: String,
-    property_version: i32,
+    property_version: i64,
     price: i64,
     seller: String,
     timestamp: chrono::NaiveDateTime,
 }
 
 impl MarketplaceOffer {
-    pub fn from_transaction(txn: &UserTransaction) -> Option<Self> {
-        let version = txn.info.version.0;
-        match txn.request.payload {
-            TransactionPayload::EntryFunctionPayload(payload) => Some(Self {
-                creator_address: payload.arguments[0]["creator"].to_string(),
-                collection_name: payload.arguments[0]["collection_name"].to_string(),
-                token_name: payload.arguments[0]["token_name"].to_string(),
-                property_version: payload.arguments[0]["property_version"]
-                    .as_i64()
-                    .unwrap()
-                    .try_into()
-                    .unwrap(),
-                price: payload.arguments[0]["price"].as_i64().unwrap(),
-                seller: txn.request.sender.inner().to_hex_literal(),
-                timestamp: parse_timestamp(txn.timestamp.0, version.try_into().unwrap()),
-            }),
+    pub fn from_table_item(
+        table_item: &WriteTableItem,
+        payload: EntryFunctionPayload,
+        txn_version: i64,
+        txn_timestamp: chrono::NaiveDateTime,
+    ) -> Result<Option<Self>> {
+        let table_item_data = &table_item.data.unwrap();
+        let maybe_offer = match MarketplaceWriteSet::from_table_item_type(
+            table_item_data.key_type.as_str(),
+            &table_item_data.value,
+            txn_version,
+        )? {
+            Some(MarketplaceWriteSet::Offer(inner)) => Some(inner),
             _ => None,
+        };
+        let maybe_list_item_payload = match MarketplacePayload::from_function_name(
+            &payload.function.to_string(),
+            &payload.arguments,
+            txn_version,
+        )
+        .unwrap()
+        {
+            Some(payload_type) => match payload_type {
+                MarketplacePayload::ListItemPayload(inner) => Some(inner),
+                _ => None,
+            },
+            None => None,
+        };
+
+        if let (Some(offer), Some(list_item_payload)) = (maybe_offer, maybe_list_item_payload) {
+            Ok(Some(Self {
+                creator_address: list_item_payload.creator,
+                collection_name: list_item_payload.collection_name,
+                token_name: list_item_payload.token_name,
+                property_version: list_item_payload.property_version,
+                price: offer.price,
+                seller: offer.seller,
+                timestamp: txn_timestamp,
+            }))
+        } else {
+            Ok(None)
         }
     }
 }
